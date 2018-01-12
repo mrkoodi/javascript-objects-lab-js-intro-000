ebe4fcda119b256ffccbc796d60457803ce41417
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-12 19:54:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object.assign({}, obeject, {[key]:value});
+  return object;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete[object];
+}
