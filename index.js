c80b969eb82b90681a13019ac36549ace5f308c8
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-12 23:05:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
- return delete Object.assign({},object,{[key]:value});
+ return delete Object.assign({},object,{[key]});
 
 }
 
