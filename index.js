e403af95770a06d1492780b7e8b38fd99ad7b6a3
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-12 22:35:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-return object.assign({},object,{[key]:value})
+return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
